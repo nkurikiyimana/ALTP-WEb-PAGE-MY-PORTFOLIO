0cd310b87dde6a6a48f729372f570c88bfac52e2
nkurikiyimana <nkurufaustin12@gmail.com>
2023-01-17 21:06:54 +0200

    $m

--- portfolio.js
+++ portfolio.js
@@ -12,3 +12,14 @@ $(window).on('scroll load', function(){
     $('#menu').removeclass('fa-times');
     $('header').removeclass('toggle');
   });
+
+
+ function adddata(){
+  var email= document.getElementById(email).value;
+  var pass= document.getElementsById(login-password).value;
+ localStorage.setItem('userEmail', email);
+ localStorage.setItem('userPwd', login-password);
+
+}
+ 
+ 
