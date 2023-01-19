51e34323ad50f26f477acf250d59b95edaf1cad3
nkurikiyimana <nkurufaustin12@gmail.com>
2023-01-19 19:23:36 +0200

    Adicionando .gitignore

--- portfolio.js
+++ portfolio.js
@@ -21,5 +21,24 @@ $(window).on('scroll load', function(){
  localStorage.setItem('userPwd', login-password);
 
 }
+function validateform(){  
+  var name=document.myform.name.value;  
+  var password=document.myform.password.value;  
+    
+  if (name==null || name==""){  
+    alert("Name can't be blank");  
+    return false;  
+  }else if(password.length<6){  
+    alert("Password must be at least 6 characters long.");  
+    return false;  
+    }  
+  }  
+  </script>  
+  <body>  
+  <form name="myform" method="post" action="http://www.javatpoint.com/javascriptpages/valid.jsp" onsubmit="return validateform()" >  
+  Name: <input type="text" name="name"><br/>  
+  Password: <input type="password" name="psrd"><br/>  
+  <input type="submit" value="register">  
+  </form>  
  
  
