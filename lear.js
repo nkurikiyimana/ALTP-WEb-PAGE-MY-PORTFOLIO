0ffa156afb7275fc96475142adb2975e2ce89362
nkurikiyimana <nkurufaustin12@gmail.com>
2023-01-25 21:15:39 +0200

    $m

---
+++ lear.js
@@ -0,0 +1,10 @@
+const container = document.querySelectorAll('div');
+console.log(container);
+ 
+const title = document.querySelector('#main-heading');
+title.style.color = 'red';
+console.log(title);
+const list = document.querySelectorAll('.list');
+
+list.style.fontsize='2rem';
+console.log(list);
